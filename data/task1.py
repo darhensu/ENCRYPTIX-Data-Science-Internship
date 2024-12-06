5a069e0fe82fd97178e1e0bfeca5f4c1ec5bbcb3
Suhendar Aditya <mochammad.120450058@student.itera.ac.id>
2024-12-06 10:15:05 +0700

    import dataset, load, and handle missing values

---
+++ data/task1.py
@@ -0,0 +1,17 @@
+import kagglehub
+import shutil
+import os
+
+# Download latest version
+path = kagglehub.dataset_download("yasserh/titanic-dataset")
+
+destination_path = r"C:\Users\user\Desktop\ENCRYPTIX Data Science Internship\data"
+
+# Buat folder tujuan jika belum adacreate destination folder if it's haven't
+os.makedirs(destination_path, exist_ok=True)
+
+# move the dataset to destination folder
+for file_name in os.listdir(path):
+    shutil.move(os.path.join(path, file_name), destination_path)
+
+print("Dataset successfully moved to:", destination_path)
